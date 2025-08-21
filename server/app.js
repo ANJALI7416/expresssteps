fae6ecf664ac9fa04140b9e35110115194731555
ANJALI7416 <ramavathanju7416@gmail.com>
2025-08-21 08:20:28 +0000

    expresssteps

---
+++ server/app.js
@@ -0,0 +1,25 @@
+import express from "express";
+
+const app = express();
+app.use(express.json()) //This line imp for post,put,delete api's
+
+const PORT = 5001;
+
+/**
+ * 1. Simple root route
+ */
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.post("/register",(req,res)=>{
+  let userData = req.body
+  console.log(userData)
+});
+
+
+// Start server
+app.listen(PORT, () => {
+  console.log(` Server running at http://localhost5001:${PORT}`);
+});
